9909326b3912b0e7a74d7449c0851a49aef3a50c
= <dhoriffitodiansyah27@gmail.com>
2023-04-12 12:34:00 +0700

    membetulkan web conflict

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,8 @@ use App\Http\Controllers\AnggotaController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JaketController;
 use App\Http\Controllers\PendakiController;
-<<<<<<< HEAD
-
-use App\Http\Controllers\SepatuController;
-
-use App\Http\Controllers\SleepingBagController;
-
-=======
 use App\Http\Controllers\SepatuController;
 use App\Http\Controllers\SleepingBagController;
->>>>>>> 1a6d954a0e5490bad2011624b11bb79051f7a616
 use App\Http\Controllers\TendaController;
 use Illuminate\Support\Facades\Route;
 
@@ -32,15 +24,7 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/anggotakelompok', [AnggotaController::class, 'index']);
 Route::resource('/datatenda', TendaController::class);
 Route::resource('/pendaki', PendakiController::class);
-<<<<<<< HEAD
-
-Route::resource('/sepatu', SepatuController::class);
-
-Route::resource('/sb', SleepingBagController::class);
-
-=======
 Route::resource('/sb', SleepingBagController::class);
 Route::resource('/jaket', JaketController::class);
 Route::resource('/sepatu', SepatuController::class);
 Route::resource('/sb', SleepingBagController::class);
->>>>>>> 1a6d954a0e5490bad2011624b11bb79051f7a616
